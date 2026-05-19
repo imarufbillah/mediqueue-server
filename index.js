a6265f22ec0a1fc2cd689ea87baf232b7615f8f1
Maruf Billah <marufbillah.dev@gmail.com>
2026-05-19 18:26:23 +0600

    feat: add API endpoint to delete tutors added by authenticated user

--- index.js
+++ index.js
@@ -98,6 +98,7 @@ async function run() {
       res.json(myTutors);
     });
 
+    // API endpoint to update a specific tutor which is added by authenticated user
     app.patch("/my-tutors/:id", validateToken, async (req, res) => {
       const id = req.params.id;
       const updatedTutor = req.body;
@@ -119,6 +120,27 @@ async function run() {
       res.json({
         success: true,
         message: "Tutor updated successfully",
+      });
+    });
+
+    app.delete("/my-tutors/:id", validateToken, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const userId = req.user.sub;
+      const tutor = await tutorsCollection.findOne(query);
+
+      if (userId !== tutor.userId) {
+        return res.status(403).json({
+          success: false,
+          message: "You are not authorized to delete this tutor",
+        });
+      }
+
+      const result = await tutorsCollection.deleteOne(query);
+      res.json({
+        success: true,
+        message: "Tutor deleted successfully",
       });
     });
 
